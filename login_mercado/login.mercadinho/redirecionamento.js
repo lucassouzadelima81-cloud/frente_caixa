0b014a1b4f8a0c9af150447689f0c04deeb50fbb
Lucas_its4 <lucassouzadelima81@gmail.com>
2026-06-17 20:02:03 -0300

    Add files via upload

---
+++ login_mercado/login.mercadinho/redirecionamento.js
@@ -0,0 +1,10 @@
+ function irParaMercadinho() {
+    window.location.href = "Mercadinho.html";
+
+    alert("você fez login com sucesso!")
+}
+
+function irParaFinalizar() {
+    window.location.href = "finaliza.html";
+    alert("finalizando compra...")
+}
